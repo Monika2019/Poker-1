28dd90a04a36811a92f5d81d58fffdd3c3befa25
Monika2019 <52123265+Monika2019@users.noreply.github.com>
2019-07-13 20:55:00 +0800

    01

---
+++ poker01.py
@@ -0,0 +1,78 @@
+# -*- coding: UTF-8 -*-
+
+import random
+import codecs
+import copy
+
+cardJokers = ('♞', '♘')
+cardMarks = ('♠', '♥', '♦', '♣')
+cardNumbers = ('2', '3', '4', '5', '6', '7', '8',
+               '9', '10', 'J', 'Q', 'K', 'A')
+
+deck = []
+for c in cardJokers:
+    deck.append(c)
+#print(deck)
+
+for cn in cardNumbers:
+    for cm in cardMarks:
+        card = cm + cn
+        deck.append(card)
+#print(deck)
+
+f = codecs.open("deck-old-54.txt", "w", "utf-8")
+for card in deck:
+    f.write(card)
+    f.write('\n')
+f.close
+
+#--------------
+'''
+shuffledDeck = []
+count = len(deck)
+for i in range(count):
+    pickedCard = random.choice(deck)
+    shuffledDeck.append(pickedCard)
+    deck.remove(pickedCard)
+
+f = codecs.open("deck-new-54.txt", "w", "utf-8")
+for card in shuffledDeck:
+    f.write(card)
+    f.write('\n')
+f.close
+'''
+# method 2:
+shuffledDeck = copy.copy(deck)
+random.shuffle(shuffledDeck)
+
+f = codecs.open("deck-new-54.txt", "w", "utf-8")
+for card in shuffledDeck:
+    f.write(card)
+    f.write('\n')
+f.close
+
+print('\n ------- Cutting line 1------')
+p1_desk=[]
+for i in range(0,19):
+    p1_desk.append(shuffledDeck[i])
+    shuffledDeck.remove(shuffledDeck[i])
+print(p1_desk)
+
+f = codecs.open("deck-player1-54.txt", "w", "utf-8")
+for a in p1_desk:
+    f.write(a)
+    f.write('\n')
+f.close
+
+
+print('\n ------- Cutting line 2 ------')
+p2_desk=[]
+for i in range(0,19):
+    p2_desk.append(shuffledDeck[i])
+print(p2_desk)
+
+f = codecs.open("deck-player2-54.txt", "w", "utf-8")
+for b in p2_desk:
+    f.write(b)
+    f.write('\n')
+f.close
